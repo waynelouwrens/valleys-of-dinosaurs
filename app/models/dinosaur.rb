7b0ed317a59abb0660404e75be536948e1d9bc24
Wayne Louwrens <wayne.louwrens@gmail.com>
2017-10-21 21:07:48 +0200

    Add Dinosaur validations

--- app/models/dinosaur.rb
+++ app/models/dinosaur.rb
@@ -1,8 +1,11 @@
 class Dinosaur < ApplicationRecord
-    scope :order_by_name, -> { order(:name) }
 
-    def is_baby?
-      age < 3
-    end
+  validates :name, presence: true
+  validates :name, uniqueness: true
+
+  validates :age, length: { maximum: 5 }, allow_blank: true
+  validates :image_url, length: { maximum: 255 }, allow_blank: true
+
+
 
 end
