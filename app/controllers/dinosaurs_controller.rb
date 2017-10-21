7d999a52e71ad82e2d636aa7237289a1ea0961fe
Wayne Louwrens <wayne.louwrens@gmail.com>
2017-10-21 21:05:07 +0200

    Edit, update, and delete

--- app/controllers/dinosaurs_controller.rb
+++ app/controllers/dinosaurs_controller.rb
@@ -22,6 +22,30 @@ class DinosaursController < ApplicationController
     end
   end
 
+  def edit
+    @dinosaur = Dinosaur.find(params[:id])
+  end
+
+  def update
+    @dinosaur = Dinosaur.find(params[:id])
+
+    if @dinosaur.update_attributes(dinosaur_params)
+      redirect_to @dinosaur
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+  @dinosaur = Dinosaur.find(params[:id])
+
+  @dinosaur.destroy
+
+  redirect_to dinosaurs_path
+end
+
+
+
   private
 
   def dinosaur_params
